056e4d9d8f4620c51d2896c8e53757c869eb860a
Lciarallo <luizeduardociarallo@gmail.com>
2023-03-30 11:37:36 -0300

    Menu main

--- src/aplicacao/Main.java
+++ src/aplicacao/Main.java
@@ -5,20 +5,38 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         JOptionPane.showInternalMessageDialog(null,"Bem vindo ao Event System!\nPor favor clique em 'ok' para continuar");
-        Object[] options = {"Discente", "Docente"};
-        int option = JOptionPane.showOptionDialog(null, "Escolha uma opção a baixo", "Cadastro", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
-        switch (option) {
-            case 0:
-                JOptionPane.showInternalMessageDialog(null, "Você selecionou Discente");
-                break;
-            case 1:
-                JOptionPane.showInternalMessageDialog(null, "Você selecionou Docente");
-                break;
+        Object[] pessoa = {"Discente", "Docente", "Sair"};
+        int op = 0;
+
+        while (op != 4){
+            String option = JOptionPane.showInputDialog(null,"Insira a opção desejada: \n" +
+                    "1 - Cadastrar Docente ou discente\n" +
+                    "2 - Cadastrar Evento\n" +
+                    "3 - Listar Eventos ou Pessoas\n" +
+                    "4 - Sair\n");
+            op = Integer.parseInt(option);
+            switch (op){
+                case 1:
+                    op = JOptionPane.showOptionDialog(null, "Escolha uma opção a baixo: ", "Cadastro", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, pessoa, pessoa[0]);
+                    switch (op) {
+                        case 0:
+                            JOptionPane.showInternalMessageDialog(null, "Você selecionou Discente");
+                            break;
+                        case 1:
+                            JOptionPane.showInternalMessageDialog(null, "Você selecionou Docente");
+                            break;
+                        case 4:
+                            break;
+
+
+                    }
+            }
+
+
+
+
+
+
         }
-
-
-
-
-
     }
 }
