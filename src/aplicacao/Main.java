c12df9f987edb7f201c152692ae2da5fd10def7c
Daniel José Martins Junior <daniel.jr2501@gmail.com>
2023-04-03 10:46:36 -0300

    03/04/2023 - Revisão e testes

--- src/aplicacao/Main.java
+++ src/aplicacao/Main.java
@@ -36,72 +36,59 @@ public class Main {
                     5 - Sair"""));
 
             switch (op) {
-                case 1:
+                case 1 -> {
                     evento.cadastrarEvento();
                     eventoCadastrado++;
-                    break;
-
-                case 2: {
+                }
+                case 2 -> {
                     int opcao = Integer.parseInt(JOptionPane.showInputDialog("""
                             Selecione o tipo de participante:
                             1 - Cadastrar um discente
                             2 - Cadastrar um docente"""));
                     switch (opcao) {
-                        case 1:
+                        case 1 -> {
                             discente.cadastrarDiscente();
                             discenteCadastrado++;
-                            break;
-                        case 2:
+                        }
+                        case 2 -> {
                             docente.cadastrarDocente();
                             docenteCadastrado++;
-                            break;
+                        }
                     }
 
-                    break;
                 }
-
-                case 3:
+                case 3 -> {
                     if (eventoCadastrado > 0) {
                         evento.mostrarEvento();
                     } else {
                         JOptionPane.showMessageDialog(null, "Não há evento cadastrado!");
                     }
-
-                    break;
-
-                case 4: {
+                }
+                case 4 -> {
                     int opcao = Integer.parseInt(JOptionPane.showInputDialog("""
                             Selecione o tipo de participante:
                             1 - Listar discente cadastrado
                             2 - Listar docente cadastrado"""));
                     switch (opcao) {
-                        case 1:
+                        case 1 -> {
                             if (discenteCadastrado > 0) {
                                 discente.mostrarDiscente();
                             } else {
                                 JOptionPane.showMessageDialog(null, "Não há discente cadastrado!");
                             }
-
-                            break;
-                        case 2:
+                        }
+                        case 2 -> {
                             if (docenteCadastrado > 0) {
                                 docente.mostrarDocente();
                             } else {
                                 JOptionPane.showMessageDialog(null, "Não há docente cadastrado!");
                             }
-
-                            break;
+                        }
                     }
-
-                    break;
                 }
-                case 5:
-                    JOptionPane.showMessageDialog(null, "Saindo...\n" +
-                            "Pressione 'ENTER' para sair.");
-                    break;
-                default:
-                    JOptionPane.showMessageDialog(null, "Opção inválida!");
-                    break;
+                case 5 -> JOptionPane.showMessageDialog(null, "Saindo...\n" +
+                        "Pressione 'ENTER' para sair.");
+                default -> JOptionPane.showMessageDialog(null, "Opção inválida!");
             }
         } while (op != 5);
     }
